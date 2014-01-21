c53d02146375dcd6ea8f6ed03530bd818c7493cc
jjuiddong <jjuiddong@gmail.com>
2014-01-21 21:31:26 +0900

    update ball move

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
+	private Vector3 m_Velocity;
+
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		m_Velocity = new Vector3 (0f, 0f, 0f);	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		Vector3 pos = m_Velocity * Time.deltaTime;
+		transform.Translate( pos );
+	}
+
+	void Shoot( Vector3 velocity )
+	{
+		m_Velocity = velocity;
 	}
 }
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Invoke ("ShootTimer", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void ShootTimer()
+	{
+		GameObject.Find("Ball").SendMessage ("Shoot", new Vector3(0,0,1));
+	}
+
+}
